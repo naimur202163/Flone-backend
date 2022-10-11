82e41c1f36a230948ad78a4a3a25bfea4a604e28
naimur <naimur202163@gmail.com>
2022-10-11 22:02:34 +0600

    working on server page

---
+++ server.js
@@ -0,0 +1,23 @@
+const app = require("./app");
+
+
+
+// handling uncaught exception
+process.on("uncaughtException",(err)=>{
+    console.log(`Error:${err.message}`);
+    console.log(`Shuting down the server due to `)
+})
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+
+
+
+
+
+const server = app.listen(5000, () => {
+  console.log(`the server is running on port 5000`);
+});

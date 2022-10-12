a66b1d33075fe2e723a9957887e1f934c564ca81
naimur <naimur202163@gmail.com>
2022-10-12 17:57:00 +0600

    category added

--- model/productModel.js
+++ model/productModel.js
@@ -1,11 +1,44 @@
 const mongoose=require('mongoose');
 
 const productSchema=mongoose.Schema({
+    sku:{
+        type:String,
+        require:true,      
+    },
         name:{
             type:String,
             require:[true,"Please Enter the Name"],
             trim:true,
-        }
+        },
+        price:{
+            type:Number,
+            required:[true,"Please Enter the price"]
+        },
+        discount:{
+            type:Number,
+            
+        },
+        offerEnd:{
+            type:String,
+        },
+        new:{
+            type:Boolean,
+        },
+        rating:{
+            type:Number,
+            required:true
+        },
+        saleCount:{
+            type:Number,
+            required:[true,"Please Enter the Sell count"]
+        },
+        category:[{
+            type:String,
+            required:true
+        }]
+
+
+       
 })
 
 

ff1224b09099fb7e7e8a0082a02380b5fb7a2e04
naimur <naimur202163@gmail.com>
2022-10-12 18:16:37 +0600

    productSehma added

--- model/productModel.js
+++ model/productModel.js
@@ -1,46 +1,74 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-const productSchema=mongoose.Schema({
-    sku:{
-        type:String,
-        require:true,      
+const productSchema = mongoose.Schema({
+  sku: {
+    type: String,
+    require: true,
+  },
+  name: {
+    type: String,
+    require: [true, "Please Enter the Name"],
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: [true, "Please Enter the price"],
+  },
+  discount: {
+    type: Number,
+  },
+  offerEnd: {
+    type: String,
+  },
+  new: {
+    type: Boolean,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  saleCount: {
+    type: Number,
+    required: [true, "Please Enter the Sell count"],
+  },
+  category: [
+    {
+      type: String,
+      required: true,
     },
-        name:{
-            type:String,
-            require:[true,"Please Enter the Name"],
-            trim:true,
+  ],
+  tag: [
+    {
+      type: String,
+      required: [true, "Please Enter the Tag"],
+    },
+  ],
+  variation: [
+    {
+      color: String,
+      image: String,
+      size: [
+        {
+          name: String,
+          stock: Number,
         },
-        price:{
-            type:Number,
-            required:[true,"Please Enter the price"]
-        },
-        discount:{
-            type:Number,
-            
-        },
-        offerEnd:{
-            type:String,
-        },
-        new:{
-            type:Boolean,
-        },
-        rating:{
-            type:Number,
-            required:true
-        },
-        saleCount:{
-            type:Number,
-            required:[true,"Please Enter the Sell count"]
-        },
-        category:[{
-            type:String,
-            required:true
-        }]
+      ],
+     
+    },
+  ],
+  images: [
+    {
+      type: String,
+    },
+  ],
+  shortDescription: {
+    type: String,
+    required:true,
+  },
+  fullDescription: {
+    type: String,
+    required: true,
+  },
+});
 
-
-       
-})
-
-
-
-module.exports=mongoose.model("Product",productSchema)
+module.exports = mongoose.model("Product", productSchema);

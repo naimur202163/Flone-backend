4b8cfa64d3a8fa154a07b51faa46112820ebc037
naimur <naimur202163@gmail.com>
2022-10-12 14:44:25 +0600

    api added

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+
+// Route Imports
+const product=require('./routes/productRoute')
+
+// Api versons
+app.use('/api/v1',product)
+
+
+
 module.exports = app;

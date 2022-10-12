6ad30030097494f4a69522ba740b7d33c8463e37
naimur <naimur202163@gmail.com>
2022-10-12 22:08:20 +0600

    user data created

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+const catchAsyncErrors = require("../middleware/catchAsyncErrors")
+const Users=require('../model/userModel')
 exports.getAllUsers=async(req,res)=>{
         res.send("User Route is wokring")
 }
@@ -5,6 +7,12 @@ exports.getAllUsers=async(req,res)=>{
 
 
 
-exports.createUsers=(req,res)=>{
-        
-}
+exports.createUsers=catchAsyncErrors(async(req,res,next)=>{
+        const users=await Users.create(req.body);
+
+        res.status(201).json({
+                success:true,
+                users
+        })
+
+})
